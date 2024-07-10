0831306faa6855af709a9a49badd6848271c83ed
Davemem <172973506+Davemem@users.noreply.github.com>
2024-07-10 20:45:58 +1000

    Lesson Complete

---
+++ java-basics/vars-operators/ext-java.js
@@ -0,0 +1,112 @@
+console.log("this is run in an ext3ernal script");
+// let message;
+// message = 'hi';
+// alert(message);
+
+// var message2 = 'bye';
+// alert(message2);
+
+// let a = '1',
+//     b = '2',
+//     c = '3';
+
+// alert(a);
+
+// const $cash = 5;
+
+// console.log($cash);
+
+// const COLOR_ORANGE = "#FF7F00";
+
+// let color = COLOR_ORANGE;
+
+// console.log(COLOR_ORANGE);
+
+// let admin;
+
+// let name = 'John';
+
+// admin = name;
+
+// alert(admin);
+
+// let earth;
+
+// let currentUser;
+
+// let mathTest = 100 % 6;
+
+// console.log(mathTest);
+
+// let x =5;
+// x--;
+// alert(x);
+
+// let y=2,
+//     z=3;
+
+// y = y ** z;
+
+// let x =10e-5;
+
+// let y = Math.pow(x,3);
+// console.log(y);
+
+// let myNumber = 2;
+
+// // while (myNumber != Infinity){
+// //     console.log(myNumber);
+// //     myNumber++;
+// // }
+
+// typeof Infinity;
+
+// myNumber.toString(myNumber);
+
+// console.log(myNumber);
+
+// const myInt = 5;
+// const myFloat = 6.667;
+
+// // console.log(myInt);
+// // console.log(myFloat);
+
+// const lotsOfDecimal = 1.763485240580294850;
+
+// console.log(lotsOfDecimal);
+
+// const twoDecimalPlaces = lotsOfDecimal.toFixed(2);
+// console.log(twoDecimalPlaces);
+
+// let myNumber = "74";
+// console.log(myNumber = Number(myNumber) + 3);
+
+// x = 4;
+
+// x+=4;
+
+// x = x +4;
+
+// !== vs !=
+
+// console.log(54 + 27);
+
+// console.log(54 + 27 +1+2+4+6);
+
+// let x = (4+6+9)/77;
+// console.log(x);
+
+// let a =10;
+// console.log(a);
+
+// const MAX = 57;
+
+// let actual = MAX -13;
+
+// let percentage = actual / MAX;
+
+let apples="2";
+let oranges ="3";
+
+alert(+apples+ +oranges);
+
